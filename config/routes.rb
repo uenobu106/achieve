973be2a84ad5a4ece5211e64273f1add9f1b05be
nobuhiro ueno <uenobu106@ezweb.ne.jp>
2017-03-20 11:32:49 +0000

    “kadai8”

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
 
 
+  devise_for :users
   resources :blogs, only: [:index, :new, :create, :edit, :update, :destroy] do
     collection do
       post :confirm
     end
    end
+  
+  
   resources :contacts, only: [:new, :create] do
     collection do
       post :confirm
