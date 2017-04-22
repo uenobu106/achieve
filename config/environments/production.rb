3180794557d914bb819a56fa46b8fca081e1513e
上野信弘 <uenobu106@uMacBook-Air.local>
2017-04-22 19:01:25 +0900

    11

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,9 +75,9 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
+  config.active_record.dump_schema_after_migration = false
 
-
-  config.action_mailer.default_url_options = { host: 'https://dry-ridge-79126.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'dry-ridge-79126.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
