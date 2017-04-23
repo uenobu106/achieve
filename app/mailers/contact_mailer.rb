7def6864ee7c3ac81fa10dfcdc33680227f6f764
上野信弘 <uenobu106@uMacBook-Air.local>
2017-04-23 14:47:37 +0900

    11

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -9,7 +9,7 @@ class NoticeMailer < ApplicationMailer
 
   def sendmail_contact(contact)
     @contact = contact
-    mail to: "contact.email",
+    mail to: @contact.email,
          subject: '【Achieve】お問い合わせが送信されました',
          form: 'no-reply@letter_opener_web.com'
   end
