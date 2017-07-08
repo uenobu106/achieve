86fa0006758d1d2d08d8980ae92e5d8865be1a13
uenobu106 <uenobu106@ezweb.ne.jp>
2017-07-09 07:19:35 +0900

    aws-2

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.6.0'
 set :application, 'achieve'
 
 # cloneするgitのレポジトリ（xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
-set :repo_url, 'https://github.com/xxxxxxxx/yyyyyyyy'
+set :repo_url, 'https://github.com/uenobu106/achieve'
 
 # deployするブランチ。デフォルトはmasterなのでなくても可。
 set :branch, ENV['BRANCH'] || 'master'
